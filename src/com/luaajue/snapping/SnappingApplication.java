e61c376312196d720a5b0212794d9e7289d6799d
Alonso Gutierrez <darkspace20@hotmail.com>
2014-06-11 20:13:29 -0400

    Upload Snapping initial files to github

---
+++ src/com/luaajue/snapping/SnappingApplication.java
@@ -0,0 +1,21 @@
+package com.luaajue.snapping;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+public class SnappingApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();  
+		Parse.initialize(this, "CR7NHAf9tp73Gx88LoXnNwp41vSXLRrgTMk1MtnG", "i2XuUGq6fUr8JJTvCDAyIc3zxZsY0hgvKq5G0xDk");
+		
+		
+	}
+	
+	
+	
+}
