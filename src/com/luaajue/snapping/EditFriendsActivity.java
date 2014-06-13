72b1dfefe1c5195de7bd68621fd56630d245fd5b
Alonso Gutierrez <darkspace20@hotmail.com>
2014-06-13 06:06:50 -0400

    Add fragments for inbox and friends, also edit friends

--- src/com/luaajue/snapping/EditFriendsActivity.java
+++ src/com/luaajue/snapping/EditFriendsActivity.java
@@ -1,29 +1,94 @@
 package com.luaajue.snapping;
 
-import android.app.Activity;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
-public class EditFriendsActivity extends Activity {
+public class EditFriendsActivity extends ListActivity {
 
+	public static final String TAG = EditFriendsActivity.class.getSimpleName();
+	
+	protected List<ParseUser> mUsers;
+	protected ParseRelation<ParseUser> mFriendsRelation;
+	protected ParseUser mCurrentUser;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // ---- for progress bar
 		setContentView(R.layout.activity_edit_friends);
 
+		setupActionBar();
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
 		
+		mCurrentUser = ParseUser.getCurrentUser();
+		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+		
+		setProgressBarIndeterminateVisibility(true);
 		ParseQuery<ParseUser> query = ParseUser.getQuery();
 		query.orderByAscending(ParseConstants.KEY_USERNAME);
 		query.setLimit(1000);
+		query.findInBackground(new FindCallback<ParseUser>() {
+			
+			@Override
+			public void done(List<ParseUser> users, ParseException e) {
+				if ( e == null){
+					setProgressBarIndeterminateVisibility(false);
+					mUsers = users;
+					String[] usernames = new String[mUsers.size()];
+					int i = 0;
+					for (ParseUser user : mUsers){
+						usernames[i] = user.getUsername();
+						i++;
+						
+					}
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this,
+							android.R.layout.simple_list_item_checked,
+							usernames);
+					setListAdapter(adapter);
+					
+					addFriendCheckmarks();
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+					AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+					builder.setMessage(e.getMessage())
+						.setTitle(R.string.error_title)
+						.setPositiveButton(android.R.string.ok, null);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				}
+				
+			}
+		});
+	}
+	
+	private void setupActionBar() {
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
 	}
 
 	@Override
@@ -46,5 +111,51 @@ public class EditFriendsActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		if (getListView().isItemChecked(position)){
+			//add friend
+			mFriendsRelation.add(mUsers.get(position));
+			mCurrentUser.saveInBackground(new SaveCallback() {
+				
+				@Override
+				public void done(ParseException e) {
+					if (e != null){
+						Log.e(TAG, e.getMessage());
+					}					
+				}
+			});
+		}
+		else{
+			//remove friend
+		}
+		
+	}
+	
+	private void addFriendCheckmarks(){		
+		mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+
+			@Override
+			public void done(List<ParseUser> friends, ParseException e) {
+				if (e == null){
+					//List was returned]]
+					for (int i = 0; i < mUsers.size(); i++){
+						ParseUser user = mUsers.get(i);
+						
+						for (ParseUser friend : friends){
+							if(friend.getObjectId().equals(user.getObjectId())){
+								getListView().setItemChecked(i, true);
+							}
+						}
+					}
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+				}								
+			}
+		});
+	}
 
 }
--- src/com/luaajue/snapping/ParseConstants.java
+++ src/com/luaajue/snapping/ParseConstants.java
@@ -7,4 +7,5 @@ public final class ParseConstants {
 	
 	//Field names
 	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "friends_relation";
 }
