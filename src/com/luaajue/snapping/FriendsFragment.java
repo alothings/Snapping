edcc2818180b15022bb1d18deaa00b1baa9c133d
Alonso Gutierrez <darkspace20@hotmail.com>
2014-06-15 04:35:59 -0400

    added camera png files.

--- src/com/luaajue/snapping/FriendsFragment.java
+++ src/com/luaajue/snapping/FriendsFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
